4ca9904f396f5a7c75d5a0c00b04ef15be704ec5
christophrus <christophrus@protonmail.com>
2019-03-27 21:43:01 +0100

    implemented solution

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
-
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
@@ -13,6 +12,22 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+
+  let ret = {};
+
+  if (req.file) {
+    ret = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    };
+  } else {
+    ret = { error: "No file uploaded" }
+  }
+  res.json(ret);
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
